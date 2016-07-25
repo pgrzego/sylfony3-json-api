8f5a04cf57c0bafcaf11f305e3b0dd3c23090dd1
Piotr Grzegorzewski <pgrzegorzewski.wenow@gmail.com>
2016-07-25 17:34:36 +0200

    Initial setup

---
+++ README.md
@@ -0,0 +1,4 @@
+.checkout
+=========
+
+A Symfony project created on July 25, 2016, 4:36 pm.
---
+++ src/AppBundle/Controller/CartController.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: piotr.grzegorzewski
+ * Date: 25/07/2016
+ * Time: 17:21
+ */
+
+namespace AppBundle\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class CartController extends Controller
+{
+
+    public function createAction() {
+
+    }
+
+    public function removeAction() {
+
+    }
+
+    public function addProductAction() {
+
+    }
+
+    public function removeProductAction() {
+
+    }
+
+    public function setProductAction() {
+
+    }
+
+    public function listProductsAction() {
+
+    }
+
+}
